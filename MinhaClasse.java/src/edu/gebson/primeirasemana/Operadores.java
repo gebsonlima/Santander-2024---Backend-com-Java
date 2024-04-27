ffc3cc526d0f9baacd7a329ca82370cabb384fa4
GebsonSilva <gebsonlima@hotmail.com>
2024-04-26 22:24:12 -0300

    continuação estudos de variaveis

---
+++ MinhaClasse.java/src/edu/gebson/primeirasemana/Operadores.java
@@ -0,0 +1,45 @@
+package edu.gebson.primeirasemana;
+
+public class Operadores {
+    public static void main(String [] args) {
+    //com operador ternario
+
+    int a, b;
+    a = 6;
+    b = 6;
+
+    String resultado = a==b ? "verdadeiro" : "falso";
+
+    System.out.println(resultado);
+
+
+
+    /* sem operador ternario
+    int a, b;
+
+      a = 5;
+      b = 6;
+
+      String resultado = "";
+    if(a==b)
+        resultado = "verdadeiro";
+    else
+        resultado = "falso";
+
+    System.out.println(resultado);
+        */
+      
+      /*  int numero = 5;
+       
+       //x repetição
+       numero++;
+       //numero = numero +1;
+
+       System.out.println( ++ numero );
+
+       //mesma regra para decrementar --
+     
+       */
+    }
+    
+}
--- MinhaClasse.java/src/edu/gebson/primeirasemana/TiposVariaveis.java
+++ MinhaClasse.java/src/edu/gebson/primeirasemana/TiposVariaveis.java
@@ -2,4 +2,25 @@ package edu.gebson.primeirasemana;
 
 public class TiposVariaveis {
     
+    public static void main(String [] args) throws Exception {
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 55750000; //se começar com zero, pode-se usar o tipo string
+        long cpf = 98541568711L; //se começar com zero, pode-se utilizar outro tipo. precisa ser encerrado com L.
+        float pi = 3.14f; //float se colocar a letra f no fim. 
+        double salario = 1275.33;
+
+        final double VALOR_DE_PI = 3.14;
+    }
+
+    //int, byte, short, long, float, double, boolean e char - não são considerados objetos - represetam valores brutos. Armazenados na pilha de memoria. (Memory stack)
+
+    //byte - 1 byte - valor minimo -128
+    //short - 2 byte - -32.768
+    //int - 4 bytes - -2.147.483.648 - mais utilizado
+    //long - 8 bytes - -9.223.372.036.854.775.808
+    
+    //tipos que podem conter partes fracionadas
+    //float - 4 bytes - -3,4028E + 38
+    //double - 8 bytes - -1,7976E 308  - mais comum em projetos. 
 }
